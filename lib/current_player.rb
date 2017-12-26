f815b45391a1319af1e56c89f1ea7cd8ba735f3a
Brendan O'Dea <brendan.ode4@gmail.com>
2017-12-26 04:03:43 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -4,7 +4,14 @@ def turn_count(board)
     if space == "X" || space == "O"
       turns += 1
     end
-    
   end
+end
 
+def current_player(board)
+  turns = turn_count(board)
+  if turns % 2 = 0
+    "X"
+  else
+    "O"
+  end
 end
